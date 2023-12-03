1707989dab4bb54bc0cee3fbccbc2727e06318fd
8aChristian <145133332+8aChristian@users.noreply.github.com>
2023-12-03 14:11:41 -0500

    API SEGUES V1.1

--- src/routes/user.js
+++ src/routes/user.js
@@ -1,12 +1,22 @@
-// AQUI SE CREAN TODOS LOS USUARIOS(RUTAS, OBTENER, ELIMINAR ETC)
+//Se importa el framework Express para manejar las rutas del usuario.
 const express = require("express");
-const userSchema = require("../models/user")
+
+// Se importa el esquema de usuario desde el modelo correspondiente.
+const userSchema = require("../models/user");
+
+// Se crea un router de Express para manejar las rutas de usuario.
 const router = express.Router();
-//create user 
-router.post("/users", (req,res)=> {
+
+// Ruta para crear un usuario.
+router.post("/users", (req, res) => {
+  // Se crea un nuevo usuario utilizando el esquema de usuario y los datos del cuerpo de la solicitud.
   const user = userSchema(req.body);
-  user.save().then((data)=> res.json(data))
-  .catch((error)=> res.json({ message: error }));
+  
+  // Se guarda el usuario en la base de datos.
+  user.save()
+    .then((data) => res.json(data)) // Si se guarda correctamente, se devuelve el usuario creado.
+    .catch((error) => res.json({ message: error })); // Si hay un error, se devuelve un mensaje de error.
 });
 
-module.exports=router;
+// Se exporta el router para su uso en otras partes de la aplicación.
+module.exports = router;
